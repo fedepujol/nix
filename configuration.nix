5e78a8cee7eda6b3ab44b59ba8d2a9e0bc619105
fedepujol <fpujol@proton.me>
2023-03-09 22:35:36 -0300

    fix: Format

--- configuration.nix
+++ configuration.nix
@@ -5,151 +5,151 @@
 { config, pkgs, ... }:
 
 {
-	imports = [
-		# Include the results of the hardware scan.
-			./hardware-configuration.nix
-	];
-	
-	nixpkgs.config.allowUnfree = true;
-	
-	boot = {
-		# Use the systemd-boot EFI boot loader
-		loader = {
-			efi.canTouchEfiVariables = false;
-			# Timeout in seconds. Use null if display should play indefinetly.
-			timeout = 5;
-			systemd-boot = {
-				enable = true;
-				# Maximum number of generations
-				configurationLimit = 5;
-				# Wheter to allow editing the kernel command-line before boot.
-				# Recommended to turn this off.
-				editor = false;
-			};
-		};
-	};
-	
-	# Set your time zone.
-	time.timeZone = "America/Argentina/Buenos_Aires";
-	i18n.defaultLocale = "en_GB.UTF-8";
-	
-	# Network
-	networking = {
-		# The global useDHCP flag is deprecated, therefore explicitly set to false here.
-		# Per-interface useDHCP will be mandatory in the future, so this generated config
-		# replicates the default behaviour.
-		useDHCP = false;
-		interfaces = {
-			enp4s0.useDHCP = true;
-			wlp3s0.useDHCP = true;
-		};
-		# hostName = "nixos"; # Define your hostname.
-		# wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-	};
-	
-	# Select internationalisation properties.
-	# i18n.defaultLocale = "en_US.UTF-8";
-	console = {
-		keyMap = "es";
-	};
-	
-	# Services
-	services = {
-		# X11 Server
-		xserver = {
-			enable = true;
-			layout = "es";
-			libinput.enable = true;
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
 
-			# GNOME DE
-			displayManager.gdm.enable = true;
-			desktopManager.gnome.enable = true;
-		};
+  nixpkgs.config.allowUnfree = true;
 
-		# Enable CUPS to print documents.
-		# printing.enable = true;
+  boot = {
+    # Use the systemd-boot EFI boot loader
+    loader = {
+      efi.canTouchEfiVariables = false;
+      # Timeout in seconds. Use null if display should play indefinetly.
+      timeout = 5;
+      systemd-boot = {
+        enable = true;
+        # Maximum number of generations
+        configurationLimit = 5;
+        # Wheter to allow editing the kernel command-line before boot.
+        # Recommended to turn this off.
+        editor = false;
+      };
+    };
+  };
 
-		# List services that you want to enable:
-		flatpak.enable = true;
-	};
+  # Set your time zone.
+  time.timeZone = "America/Argentina/Buenos_Aires";
+  i18n.defaultLocale = "en_GB.UTF-8";
 
-	# Programs
-	programs.zsh = {
-		enable = true;
-		promptInit = ''
-			PROMPT='%F{green}%n%f@%F{magenta}%m%f %F{blue}%B%~%b%f %# '
-		'';
-	};
+  # Network
+  networking = {
+    # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+    # Per-interface useDHCP will be mandatory in the future, so this generated config
+    # replicates the default behaviour.
+    useDHCP = false;
+    interfaces = {
+      enp4s0.useDHCP = true;
+      wlp3s0.useDHCP = true;
+    };
+    # hostName = "nixos"; # Define your hostname.
+    # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  };
 
-	programs.nano = {
-		nanorc = builtins.readFile(./modules/nano/.nanorc);
-		syntaxHighlight = true;
-	};
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    keyMap = "es";
+  };
 
-	# Users. Don't forget to set a password with ‘passwd’.
-	users.users.fedepujol = {
-		isNormalUser = true;
-		home = "/home/fedepujol";
-		description = "fedepujol";
-		extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
-		shell = pkgs.zsh;
-	};
+  # Services
+  services = {
+    # X11 Server
+    xserver = {
+      enable = true;
+      layout = "es";
+      libinput.enable = true;
 
-	# Users default-shell bash
-	users.defaultUserShell = pkgs.bash;
-	
-	# List packages default
-	environment = {
-		# Some programs to determine if a user is "normal" or "system"
-		# use the /etc/shells. It's recommended to add the shell.
-		shells = with pkgs; [ zsh ];
-		# System-wide packages
-		systemPackages = with pkgs; [
-			# CLI
-			gcc
-			nix-prefetch-scripts
-			(python39.withPackages(ps: with ps; [ 
-					numpy
-					matplotlib
-					python-lsp-server
-				]
-			))
-			
-			# GUI
-			firefox
-			gnome.gnome-tweaks
-			veracrypt
-			obsidian
-		];
+      # GNOME DE
+      displayManager.gdm.enable = true;
+      desktopManager.gnome.enable = true;
+    };
 
-		# Enviroment variables
-		variables.EDITOR = "nvim";
-	};
+    # Enable CUPS to print documents.
+    # printing.enable = true;
 
-	# Fonts
-	fonts.fonts = with pkgs; [
-		(nerdfonts.override {
-			fonts = [ "FiraCode" "Ubuntu" ];
-		})
-	];
-	
-	# This value determines the NixOS release from which the default
-	# settings for stateful data, like file locations and database versions
-	# on your system were taken. It‘s perfectly fine and recommended to leave
-	# this value at the release version of the first install of this system.
-	# Before changing this value read the documentation for this option
-	# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-	system.stateVersion = "21.11"; # Did you read the comment?
-	
-	nix = {
-		# Automatically run the garbage collector
-		gc.automatic = true;
-		
-		# Automatically detects files in the store with identical contents,
-		# and replaces them with hard links to a single copy.
-		settings.auto-optimise-store = true;
-		package = pkgs.nixFlakes;
-		extraOptions = "experimental-features = nix-command flakes";
-	};
+    # List services that you want to enable:
+    flatpak.enable = true;
+  };
+
+  # Programs
+  programs.zsh = {
+    enable = true;
+    promptInit = ''
+      			PROMPT='%F{green}%n%f@%F{magenta}%m%f %F{blue}%B%~%b%f %# '
+      		'';
+  };
+
+  programs.nano = {
+    nanorc = builtins.readFile (./modules/nano/.nanorc);
+    syntaxHighlight = true;
+  };
+
+  # Users. Don't forget to set a password with ‘passwd’.
+  users.users.fedepujol = {
+    isNormalUser = true;
+    home = "/home/fedepujol";
+    description = "fedepujol";
+    extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
+    shell = pkgs.zsh;
+  };
+
+  # Users default-shell bash
+  users.defaultUserShell = pkgs.bash;
+
+  # List packages default
+  environment = {
+    # Some programs to determine if a user is "normal" or "system"
+    # use the /etc/shells. It's recommended to add the shell.
+    shells = with pkgs; [ zsh ];
+    # System-wide packages
+    systemPackages = with pkgs; [
+      # CLI
+      gcc
+      nix-prefetch-scripts
+      (python310.withPackages (ps: with ps; [
+        numpy
+        matplotlib
+      ]
+      ))
+
+      # GUI
+      firefox
+      gnome.gnome-tweaks
+      veracrypt
+      obsidian
+      rustup
+    ];
+
+    # Enviroment variables
+    variables.EDITOR = "nvim";
+  };
+
+  # Fonts
+  fonts.fonts = with pkgs; [
+    (nerdfonts.override {
+      fonts = [ "FiraCode" "Ubuntu" ];
+    })
+  ];
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "21.11"; # Did you read the comment?
+
+  nix = {
+    # Automatically run the garbage collector
+    gc.automatic = true;
+
+    # Automatically detects files in the store with identical contents,
+    # and replaces them with hard links to a single copy.
+    settings.auto-optimise-store = true;
+    package = pkgs.nixFlakes;
+    extraOptions = "experimental-features = nix-command flakes";
+  };
 }
 
