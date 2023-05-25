c976520addc61726e14886173ba7a4850e6acd43
fedepujol <fpujol@proton.me>
2023-05-24 22:30:08 -0300

    feat: move to nixos-unstable

--- flake.nix
+++ flake.nix
@@ -1,38 +1,40 @@
 {
-	description = "A very basic flake";
+  description = "system config";
 
-	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
-		home-manager = {
-			url = "github:nix-community/home-manager/release-22.11";
-				inputs.nixpkgs.follows = "nixpkgs";
-		};
-	};
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-	outputs = { self, nixpkgs, home-manager }:
-		let
-			system = "x86_64-linux";
-			pkgs = import nixpkgs {
-				inherit system;
-				config.allowUnfree = true;
-			};
-			lib = nixpkgs.lib;
-		in {
-			nixosConfigurations = {
-				nixos = lib.nixosSystem {
-					inherit system;
-					modules = [
-						./configuration.nix
-						home-manager.nixosModules.home-manager {
-							home-manager.useGlobalPkgs = true;
-							home-manager.useUserPackages = true;
-							home-manager.users.fedepujol = {
-								imports = [ ./home.nix ];
-							};
-						}
-					];
-				};
-			};
-		};
+  outputs = { self, nixpkgs, home-manager }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      lib = nixpkgs.lib;
+    in
+    {
+      nixosConfigurations = {
+        nixos = lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.fedepujol = {
+                imports = [ ./home.nix ];
+              };
+            }
+          ];
+        };
+      };
+    };
 }
 
