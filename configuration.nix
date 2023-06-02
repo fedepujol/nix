d13809781313e097cd2c225e0a32ac1627771534
fedepujol <fpujol@proton.me>
2023-06-02 19:06:53 -0300

    feat: add cachix for Hyprland

--- configuration.nix
+++ configuration.nix
@@ -150,7 +150,11 @@
 
     # Automatically detects files in the store with identical contents,
     # and replaces them with hard links to a single copy.
-    settings.auto-optimise-store = true;
+    settings = {
+      auto-optimise-store = true;
+      substituters = [ "https://hyprland.cachix.org" ];
+      trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+    };
     package = pkgs.nixFlakes;
     extraOptions = "experimental-features = nix-command flakes";
   };
